11e87883698d09b032ec5e68fda7bbb2bbfe29c0
arnaud <arnaud.degiuli@gmail.com>
2013-07-02 09:47:28 +0200

    Improve the heat image: depending the temperature the image is different.

--- android/src/main/java/com/adgsoftware/mydomo/ui/components/HeatingComponent.java
+++ android/src/main/java/com/adgsoftware/mydomo/ui/components/HeatingComponent.java
@@ -14,18 +14,21 @@ import com.adgsoftware.mydomo.engine.controller.DimensionStatus;
 import com.adgsoftware.mydomo.engine.controller.DimensionStatusCallback;
 import com.adgsoftware.mydomo.engine.controller.Status;
 import com.adgsoftware.mydomo.engine.controller.heating.HeatingZone;
+import com.adgsoftware.mydomo.engine.controller.heating.dimension.DesiredTemperature;
 import com.adgsoftware.mydomo.engine.controller.heating.dimension.MeasureTemperature;
 
 public class HeatingComponent extends AbstractComponent {
 	
 	private SeekBar seekBar;
 	private ImageButton heat;
+	private ImageButton heatMode;
 	private TextView txtTemp;
 
 	public HeatingComponent(Context context, final HeatingZone heating) {
 		super(context);
 		seekBar = new SeekBar(context);
 		heat = new ImageButton(context);
+		heatMode = new ImageButton(context);
 		txtTemp = new TextView(context);
 		txtTemp.setText("...");
 		
@@ -34,6 +37,7 @@ public class HeatingComponent extends AbstractComponent {
 //		this.addView(seekBar);
 		this.addView(heat);
 		this.addView(txtTemp);
+		this.addView(heatMode);
 		
 		LayoutParams params = new LayoutParams(android.widget.LinearLayout.LayoutParams.MATCH_PARENT, android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
 		this.setLayoutParams(params);
@@ -48,13 +52,23 @@ public class HeatingComponent extends AbstractComponent {
 			public void value(MeasureTemperature arg0) {
 				if (arg0 != null) {
 					double temperature = arg0.getMeasuredTemperature(); 
-					setTemperaturImage(temperature);
+					setTemperatureImage(temperature);
 					txtTemp.setText("" + temperature);
 				}
 			}
 		});
-		
-		
+
+//		heating.getStatus(arg0)getDesiredTemperature(new DimensionStatusCallback<DesiredTemperature>() {
+//			
+//			@Override
+//			public void value(DesiredTemperature arg0) {
+//				if (arg0 != null) {
+//					int mode = arg0.getMode(); 
+					setModeImage(1);
+//				}
+//			}
+//		});
+//		
 		
 //		final OnSeekBarChangeListener onSeekBarChangeListener = new OnSeekBarChangeListener() {
 //
@@ -103,7 +117,7 @@ public class HeatingComponent extends AbstractComponent {
 			@Override
 			public void onDimensionChange(ControllerDimension<? extends Status> arg0,
 					DimensionStatus arg1) {
-				setTemperaturImage(((MeasureTemperature) arg1).getMeasuredTemperature());
+				setTemperatureImage(((MeasureTemperature) arg1).getMeasuredTemperature());
 				
 			}
 		});
@@ -114,8 +128,26 @@ public class HeatingComponent extends AbstractComponent {
 		return heat; // TODO a quoi ca sert?
 	}
 
-	private void setTemperaturImage(double temp) {
+	private void setTemperatureImage(double temp) {
 		// TODO manage the system (heat? cold?...  on/off ....)
-		heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat);
+		if (temp < 2.00) {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_0);
+		} else if (temp < 10.00) {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_1);
+		} else if (temp < 15.00) {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_2);
+		} else if (temp < 20.00) {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_3);
+		} else if (temp < 30.00) {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_4);
+		} else {
+			heat.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat_5);
+		}
 	}	
+	
+	private void setModeImage(int mode) {
+		if (mode == 1) {
+			heatMode.setImageResource(com.adgsoftware.mydomo.R.drawable.ic_heat);
+		}
+	}
 }
