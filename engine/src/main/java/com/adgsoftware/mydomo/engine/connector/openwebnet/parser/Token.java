4832e39ce748aee16ae2e41d23a1ecfc86abf4e8
Arnaud de Giuli <adegiuli@GENEVE.LAPADEGIULI>
2013-12-15 21:01:38 +0100

    Add licence.

--- engine/src/main/java/com/adgsoftware/mydomo/engine/connector/openwebnet/parser/Token.java
+++ engine/src/main/java/com/adgsoftware/mydomo/engine/connector/openwebnet/parser/Token.java
@@ -2,6 +2,29 @@
 /* JavaCCOptions:TOKEN_EXTENDS=,KEEP_LINE_COL=null,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package com.adgsoftware.mydomo.engine.connector.openwebnet.parser;
 
+/*
+ * #%L
+ * MyDomoEngine
+ * %%
+ * Copyright (C) 2011 - 2013 A. de Giuli
+ * %%
+ * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * 
+ *     MyDomo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     MyDomo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 /**
  * Describes the input token stream.
  */
