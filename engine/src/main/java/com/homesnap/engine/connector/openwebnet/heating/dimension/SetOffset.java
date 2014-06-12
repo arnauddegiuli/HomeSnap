98597c0253542d0747d0a52c0ec14f74b4b7b3c0
Arnaud de Giuli <arnaud.degiuli@free.fr>
2014-06-12 22:53:29 +0200

    Update file header.

--- engine/src/main/java/com/homesnap/engine/connector/openwebnet/heating/dimension/SetOffset.java
+++ engine/src/main/java/com/homesnap/engine/connector/openwebnet/heating/dimension/SetOffset.java
@@ -10,24 +10,25 @@ import com.homesnap.engine.controller.what.StateValue;
 
 /*
  * #%L
- * MyDomoEngine
+ * HomeSnapEngine
  * %%
- * Copyright (C) 2011 - 2013 A. de Giuli
+ * Copyright (C) 2011 - 2014 A. de Giuli
  * %%
- * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * This file is part of HomeSnap done by Arnaud de Giuli (arnaud.degiuli(at)free.fr)
+ *     helped by Olivier Driesbach (olivier.driesbach(at)gmail.com).
  * 
- *     MyDomo is free software: you can redistribute it and/or modify
+ *     HomeSnap is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  * 
- *     MyDomo is distributed in the hope that it will be useful,
+ *     HomeSnap is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  * 
  *     You should have received a copy of the GNU General Public License
- *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ *     along with HomeSnap. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
