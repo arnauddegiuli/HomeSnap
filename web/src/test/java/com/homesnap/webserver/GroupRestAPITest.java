0d278e94b6f8332e150114b0295c5f8e9b2c46ca
Arnaud de Giuli <arnaud.degiuli@free.fr>
2014-09-25 18:35:04 +0200

    Refactor API REST Test

--- web/src/test/java/com/homesnap/webserver/GroupRestAPITest.java
+++ web/src/test/java/com/homesnap/webserver/GroupRestAPITest.java
@@ -1,16 +1,38 @@
 package com.homesnap.webserver;
-import java.io.File;
-import java.io.IOException;
 
+/*
+ * #%L
+ * HomeSnapWebServer
+ * %%
+ * Copyright (C) 2011 - 2014 A. de Giuli
+ * %%
+ * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * 
+ *     MyDomo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     MyDomo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import com.homesnap.engine.controller.light.stateValue.LightStatusValue;
+import com.homesnap.engine.controller.who.Who;
 
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -18,110 +40,17 @@ public class GroupRestAPITest extends AbstractRestApi {
 
 	private static final String urn_groups = "/house/groups";
 
-	@Before
-	public void init() {
-		try {
-			copyFileUsingFileChannels(new File("Backup of house.xml"), new File("house.xml"));
-		} catch (IOException e) {
-			Assert.fail("Impossible to initialize file");
-		}
-	}
+//	@Before
+//	public void init() {
+//		try {
+//			copyFileUsingFileChannels(new File("Backup of house.xml"), new File("house.xml"));
+//		} catch (IOException e) {
+//			Assert.fail("Impossible to initialize file");
+//		}
+//	}
 
 	@Test
-	public void aCreateGroupList() {
-		// Test impossible to create grouplist
-		postRequestJSONObject(urn_groups,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
-	}
-
-	@Test
-	public void bPutGroupList() {
-		// Test impossible to create grouplist
-		putRequestJSONObject(urn_groups,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
-	}
-
-	@Test
-	public void cgetGroupList() {
-		// Test to get groups list
-		JSONArray groups = getRequestJSONArray(urn_groups);
-
-		// Test groups
-		testGroup2(groups.getJSONObject(1));
-	}	
-	
-	@Test
-	public void fModifyGroup() {
-		// Test to get a specific group (TODO no add or remove by this methode... should be done unit by unit...)
-		JSONObject group = putRequestJSONObject(urn_groups + "/4", createGroup4(), HttpServletResponse.SC_OK);
-		// Test group 1
-		testGroup4(group);
-
-		group = putRequestJSONObject(urn_groups + "/group?id=5", createGroup5(), HttpServletResponse.SC_OK);
-		// Test group 1
-		testGroup5(group);
-	}
-
-
-
-
-
-
-	@Test
-	public void fOnStatus() {
-		// Test to get a specific group
-		JSONObject group = getRequestJSONObject("/house/groups/2?param=param");
-		// Test group 1
-		testGroup2(group);
-
-		group = getRequestJSONObject("/house/groups/group?id=2&param=param");
-		// Test group 1
-		testGroup2(group);
-		
-		JSONObject jo = getRequestJSONObject("/house/groups/2/controller?id=21&param=param");
-		testController21(jo);
-
-		jo = getRequestJSONObject("/house/groups/2/controller/21?param=param");
-		testController21(jo);
-		
-		// Test to get a controller of type light!
-		jo = getRequestJSONObject("/house/labels/ch1/controller?id=61&param=param");
-		testController61(jo);
-		
-		jo = getRequestJSONObject("/house/labels/ch1/controller/61?param=param");
-		testController61(jo);
-		
-		jo = getRequestJSONObject("/house/controllers/21?param=param");
-		testController21(jo);
-		
-		jo = getRequestJSONObject("/house/controllers/controller?id=21&param=param");
-		testController21(jo);
-
-		jo = getRequestJSONObject("/house/controllers/controller/21?param=param");
-		testController21(jo);
-
-		putRequestJSONObject("/house/groups/1?param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		putRequestJSONObject("/house/groups/group?id=1&param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-
-		postRequestJSONObject("/house/groups/1?param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		postRequestJSONObject("/house/groups/group?id=1&param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-
-		deleteRequestJSONObject("/house/groups/1?param=param", HttpServletResponse.SC_NOT_IMPLEMENTED);
-		deleteRequestJSONObject("/house/groups/group?id=1&param=param", HttpServletResponse.SC_NOT_IMPLEMENTED);
-	}
-
-	@Test
-	public void uDeleteGroup() {
-		// Test to get a specific group
-		JSONObject group = deleteRequestJSONObject(urn_groups + "/6", HttpServletResponse.SC_OK);
-		// Test group 1
-		testGroup6(group);
-
-		group = deleteRequestJSONObject(urn_groups + "/group?id=7", HttpServletResponse.SC_OK);
-		// Test group 1
-		testGroup7(group);
-	}
-
-	@Test
-	public void vDeleteGroupList() {
+	public void test0DeleteGroupList() {
 		// Test impossible to create grouplist
 		JSONObject jo = deleteRequestJSONObject(urn_groups, HttpServletResponse.SC_NO_CONTENT);
 		Assert.assertNull(jo);
@@ -130,12 +59,24 @@ public class GroupRestAPITest extends AbstractRestApi {
 		JSONArray controllers = getRequestJSONArray(urn_groups);
 		Assert.assertEquals(0, controllers.length());
 	}
+	
+	@Test
+	public void test1CreateGroupList() {
+		// Test impossible to create grouplist
+		postRequestJSONObject(urn_groups,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
+	}
 
 	@Test
-	public void wcreateGroup() {
+	public void test2PutGroupList() {
+		// Test impossible to create grouplist
+		putRequestJSONObject(urn_groups,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
+	}
+	
+	@Test
+	public void test3CreateGroup() {
 		// Test to get a specific group
 		JSONObject group = postRequestJSONObject(urn_groups + "/6", createGroup6(), HttpServletResponse.SC_CREATED);
-		// Test group 1
+		// Test group 6
 		testGroup6(group);
 
 		group = postRequestJSONObject(urn_groups + "/group?id=10", createGroup10(), HttpServletResponse.SC_CREATED);
@@ -148,17 +89,137 @@ public class GroupRestAPITest extends AbstractRestApi {
 	}
 
 	@Test
-	public void zgetGroup() {
+	public void test4GetGroup() {
 		// Test to get a specific group
 		JSONObject group = getRequestJSONObject(urn_groups + "/1");
 		// Test group 1
 		Assert.assertNotNull(group);
 		testGroup1(group);
 
-		group = getRequestJSONObject(urn_groups + "/group?id=1");
-		// Test group 1
+		group = getRequestJSONObject(urn_groups + "/group?id=10");
+		// Test group 10
 		Assert.assertNotNull(group);
-		testGroup1(group);
+		testGroup10(group);
+	}
+	@Test
+	public void test5GetGroupList() {
+		// Test to get groups list
+		JSONArray groups = getRequestJSONArray(urn_groups);
+
+		// Test groups
+		testGroup10(groups.getJSONObject(1));
+	}	
+	
+	@Test
+	public void test6ModifyGroup() {
+		// Test to get a specific group (TODO no add or remove by this methode... should be done unit by unit...)
+		JSONObject group = putRequestJSONObject(urn_groups + "/1", createGroup1Bis(), HttpServletResponse.SC_OK);
+		// Test group 1
+		testGroup1Bis(group);
+
+		group = putRequestJSONObject(urn_groups + "/group?id=6", createGroup6Bis(), HttpServletResponse.SC_OK);
+		// Test group 1
+		testGroup6Bis(group);
 	}
 
+	@Test
+	public void test7OnStatus() {
+		// Test to get a specific group
+		JSONObject group = getRequestJSONObject("/house/groups/1?param=param");
+		// Test group 1
+		testGroup1Bis(group);
+
+		group = getRequestJSONObject("/house/groups/group?id=10&param=param");
+		// Test group 10
+		testGroup10(group);
+		
+		
+		putRequestJSONObject("/house/groups/1?param=param", createGroup1(), HttpServletResponse.SC_OK);
+		putRequestJSONObject("/house/groups/group?id=1&param=param", createGroup1(), HttpServletResponse.SC_OK);
+		
+		postRequestJSONObject("/house/groups/1/21", createController21(), HttpServletResponse.SC_CREATED);
+		JSONObject jo = getRequestJSONObject("/house/groups/1/controller?id=21&param=param");
+		testController21(jo);
+
+		jo = getRequestJSONObject("/house/groups/1/21?param=param");
+		testController21(jo);
+		
+
+		testController21(jo);
+
+		deleteRequestJSONObject("/house/groups/1?param=param", HttpServletResponse.SC_OK);
+		deleteRequestJSONObject("/house/groups/group?id=1&param=param", HttpServletResponse.SC_NOT_ACCEPTABLE);
+	}
+
+	@Test
+	public void test8DeleteGroup() {
+		// Test to get a specific group
+		JSONObject group = deleteRequestJSONObject(urn_groups + "/10", HttpServletResponse.SC_OK);
+		// Test group 10
+		testGroup10(group);
+
+		group = deleteRequestJSONObject(urn_groups + "/group?id=6", HttpServletResponse.SC_OK);
+		// Test group 10
+		testGroup6Bis(group);
+	}
+
+	@Test
+	public void test9DeleteGroupList() {
+		// Test impossible to create grouplist
+		JSONObject jo = deleteRequestJSONObject(urn_groups, HttpServletResponse.SC_NO_CONTENT);
+		Assert.assertNull(jo);
+
+		// Test there is no more controllers
+		JSONArray controllers = getRequestJSONArray(urn_groups);
+		Assert.assertEquals(0, controllers.length());
+	}
+
+	
+	private String createController21() {
+		return createJsonController("Bureau", "21", Who.LIGHT.name());
+	}
+	
+	private void testController21(JSONObject jo) {
+		testController(jo, "Bureau", "21", Who.LIGHT.name(), LightStatusValue.LIGHT_OFF.name());
+	}
+	
+	private String createGroup1() {
+		return createJsonGroup("Group 1", "1", "");
+	}
+
+	private void testGroup1(JSONObject group) {
+		testGroup(group, "1", "Group 1", 0);
+	}
+	
+	private String createGroup1Bis() {
+		return createJsonGroup("Group Bis 1", "1", "");
+	}
+
+	private void testGroup1Bis(JSONObject group) {
+		testGroup(group, "1", "Group Bis 1", 0);
+	}
+	
+	private String createGroup6() {
+		return createJsonGroup("Group 6", "6", "");
+	}
+
+	private void testGroup6(JSONObject group) {
+		testGroup(group, "6", "Group 6", 0);
+	}
+
+	private String createGroup6Bis() {
+		return createJsonGroup("Group 6 Bis", "6", "");
+	}
+
+	private void testGroup6Bis(JSONObject group) {
+		testGroup(group, "6", "Group 6 Bis", 0);
+	}
+	
+	private String createGroup10() {
+		return createJsonGroup("Group 10", "10", "");
+	}
+
+	private void testGroup10(JSONObject group) {
+		testGroup(group, "10", "Group 10", 0);
+	}
 }
--- web/src/test/java/com/homesnap/webserver/LabelRestAPITest.java
+++ web/src/test/java/com/homesnap/webserver/LabelRestAPITest.java
@@ -1,4 +1,27 @@
 package com.homesnap.webserver;
+
+/*
+ * #%L
+ * HomeSnapWebServer
+ * %%
+ * Copyright (C) 2011 - 2014 A. de Giuli
+ * %%
+ * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * 
+ *     MyDomo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     MyDomo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 import java.io.File;
 import java.io.IOException;
 
@@ -8,9 +31,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.homesnap.engine.controller.who.Who;
 
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LabelRestAPITest extends AbstractRestApi {
 
 	private static final String urn_labels = "/house/labels";
@@ -24,36 +52,20 @@ public class LabelRestAPITest extends AbstractRestApi {
 	}
 
 	@Test
-	public void createLabelList() {
+	public void test1DeleteLabelList() {
+		// Test impossible to create label list
+		JSONObject result = deleteRequestJSONObject(urn_labels, HttpServletResponse.SC_NO_CONTENT);
+		Assert.assertNull(result);
+	}
+	
+	@Test
+	public void test2CreateLabelList() {
 		// Test impossible to create label list
 		postRequestJSONObject(urn_labels,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
 	}
 
 	@Test
-	public void getLabelList() {
-		// Test to get labels list
-		JSONArray labels = getRequestJSONArray(urn_labels);
-
-		// Test labels
-		testLabelCh1(labels.getJSONObject(0));
-		testLabelCh2(labels.getJSONObject(1));
-	}
-
-	@Test
-	public void modifyLabelList() {
-		// Test impossible to modify label list
-		putRequestJSONObject(urn_labels,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
-	}
-
-	@Test
-	public void deleteLabelList() {
-		// Test impossible to create label list
-		JSONObject result = deleteRequestJSONObject(urn_labels, HttpServletResponse.SC_ACCEPTED);
-		Assert.assertNull(result);
-	}
-
-	@Test
-	public void createLabel() {
+	public void test3CreateLabel() {
 		// Test new label creation
 		JSONObject label = postRequestJSONObject(urn_labels + "/ch6", createJsonLabelCh6(), HttpServletResponse.SC_CREATED);
 		testLabelCh6(label);
@@ -61,58 +73,112 @@ public class LabelRestAPITest extends AbstractRestApi {
 		label = postRequestJSONObject(urn_labels + "/label?id=ch7", createJsonLabelCh7(), HttpServletResponse.SC_CREATED);
 		testLabelCh7(label);
 	}
-
+	
 	@Test
-	public void getLabel() {
-		// Test to get a specific label
-		JSONObject label = getRequestJSONObject(urn_labels + "/ch1");
-		// Test label Ch1
-		testLabelCh1(label);
+	public void test4GetLabelList() {
+		// Test to get labels list
+		JSONArray labels = getRequestJSONArray(urn_labels);
 
-		label = getRequestJSONObject(urn_labels + "/label?id=ch1");
-		// Test label Ch1
-		testLabelCh1(label);
+		// Test labels
+		testLabelCh6(labels.getJSONObject(0));
+		testLabelCh7(labels.getJSONObject(1));
 	}
 
 	@Test
-	public void updateLabel() {
-		// Test new label creation
-		JSONObject label = putRequestJSONObject(urn_labels + "/ch6", createJsonLabelCh6(), HttpServletResponse.SC_OK);
+	public void test5ModifyLabelList() {
+		// Test impossible to modify label list
+		putRequestJSONObject(urn_labels,"{}", HttpServletResponse.SC_NOT_IMPLEMENTED);
+	}
+
+	@Test
+	public void test6GetLabel() {
+		// Test to get a specific label
+		JSONObject label = getRequestJSONObject(urn_labels + "/ch6");
+		// Test label Ch1
 		testLabelCh6(label);
-		
-		label = putRequestJSONObject(urn_labels + "/label?id=ch7", createJsonLabelCh7(), HttpServletResponse.SC_OK);
+
+		label = getRequestJSONObject(urn_labels + "/label?id=ch7");
+		// Test label Ch1
 		testLabelCh7(label);
 	}
 
 	@Test
-	public void deleteLabel() {
+	public void test7UpdateLabel() {
+		// Test new label creation
+		JSONObject label = putRequestJSONObject(urn_labels + "/ch6", createJsonLabelCh6Bis(), HttpServletResponse.SC_OK);
+		testLabelCh6Bis(label);
+		
+		label = putRequestJSONObject(urn_labels + "/label?id=ch7", createJsonLabelCh7Bis(), HttpServletResponse.SC_OK);
+		testLabelCh7Bis(label);
+	}
+
+	@Test
+	public void test8OnStatus() {
+		
+		// Test to get a specific label
+		JSONObject label = getRequestJSONObject("/house/labels/ch6?param=param");
+		// Test label Ch1
+		testLabelCh6Bis(label);
+
+		label = getRequestJSONObject("/house/labels/label?id=ch7&param=param");
+		// Test label Ch1
+		testLabelCh7Bis(label);
+		
+		putRequestJSONObject("/house/labels/ch1?param=param", createJsonController17(), HttpServletResponse.SC_NOT_ACCEPTABLE); // Ch1 not existing!
+		putRequestJSONObject("/house/labels/label?id=ch1&param=param", createJsonController17(), HttpServletResponse.SC_NOT_ACCEPTABLE);
+		
+		postRequestJSONObject("/house/labels/ch7?param=param", createJsonLabelCh7(), HttpServletResponse.SC_NOT_ACCEPTABLE);
+		postRequestJSONObject("/house/labels/label?id=ch7&param=param", createJsonLabelCh7(), HttpServletResponse.SC_NOT_ACCEPTABLE);
+		
+		deleteRequestJSONObject("/house/labels/ch7?param=param", HttpServletResponse.SC_OK);
+		deleteRequestJSONObject("/house/labels/label?id=ch7&param=param", HttpServletResponse.SC_NOT_ACCEPTABLE);
+	}
+	
+	@Test
+	public void test9DeleteLabel() {
 		// Test new label creation
 		JSONObject label = deleteRequestJSONObject(urn_labels + "/ch6", HttpServletResponse.SC_OK);
-		testLabelCh6(label);
+		testLabelCh6Bis(label);
 		
-		label = deleteRequestJSONObject(urn_labels + "/label?id=ch7", HttpServletResponse.SC_OK);
-		testLabelCh7(label);
+		label = deleteRequestJSONObject(urn_labels + "/label?id=ch7", HttpServletResponse.SC_NOT_ACCEPTABLE);
+		Assert.assertNull(label);
 	}
 
-	@Test
-	public void onStatus() {
-		
-		// Test to get a specific label
-		JSONObject label = getRequestJSONObject("/house/labels/ch1?param=param");
-		// Test label Ch1
-		testLabelCh1(label);
 
-		label = getRequestJSONObject("/house/labels/label?id=ch1&param=param");
-		// Test label Ch1
-		testLabelCh1(label);
-		
-		putRequestJSONObject("/house/labels/ch1?param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		putRequestJSONObject("/house/labels/label?id=ch1&param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		
-		postRequestJSONObject("/house/labels/ch1?param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		postRequestJSONObject("/house/labels/label?id=ch1&param=param", createJsonController17(), HttpServletResponse.SC_NOT_IMPLEMENTED);
-		
-		deleteRequestJSONObject("/house/labels/ch1?param=param", HttpServletResponse.SC_NOT_IMPLEMENTED);
-		deleteRequestJSONObject("/house/labels/label?id=ch1&param=param", HttpServletResponse.SC_NOT_IMPLEMENTED);
+	
+	private String createJsonLabelCh6() {
+		return createJsonLabel("Chambre 6", "ch6");
+	}
+
+	private void testLabelCh6(JSONObject label) {
+		testLabel(label, "ch6", "Chambre 6", 0);
+	}
+
+	private String createJsonLabelCh6Bis() {
+		return createJsonLabel("Chambre 6 Bis", "ch6");
+	}
+
+	private void testLabelCh6Bis(JSONObject label) {
+		testLabel(label, "ch6", "Chambre 6 Bis", 0);
+	}
+	
+	private String createJsonLabelCh7() {
+		return createJsonLabel("Chambre 7", "ch7");
+	}
+
+	private void testLabelCh7(JSONObject label) {
+		testLabel(label, "ch7", "Chambre 7", 0);
+	}
+	
+	private String createJsonLabelCh7Bis() {
+		return createJsonLabel("Chambre 7 Bis", "ch7");
+	}
+
+	private void testLabelCh7Bis(JSONObject label) {
+		testLabel(label, "ch7", "Chambre 7 Bis", 0);
+	}
+	
+	private String createJsonController17() {
+		return createJsonController("Test", "17", Who.LIGHT.name());
 	}
 }
