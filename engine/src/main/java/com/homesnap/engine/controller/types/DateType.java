f4532a55c97270913aa0bf894851116da8878fe6
Arnaud de Giuli <arnaud.degiuli@free.fr>
2014-10-19 21:59:30 +0200

    Update pom and license update.

--- engine/src/main/java/com/homesnap/engine/controller/types/DateType.java
+++ engine/src/main/java/com/homesnap/engine/controller/types/DateType.java
@@ -1,5 +1,30 @@
 package com.homesnap.engine.controller.types;
 
+/*
+ * #%L
+ * HomeSnapEngine
+ * %%
+ * Copyright (C) 2011 - 2014 A. de Giuli
+ * %%
+ * This file is part of HomeSnap done by Arnaud de Giuli (arnaud.degiuli(at)free.fr)
+ *     helped by Olivier Driesbach (olivier.driesbach(at)gmail.com).
+ * 
+ *     HomeSnap is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     HomeSnap is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with HomeSnap. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+
 import java.util.Locale;
 
 /**
--- web/src/main/java/com/homesnap/webserver/rest/parser/Token.java
+++ web/src/main/java/com/homesnap/webserver/rest/parser/Token.java
@@ -2,6 +2,29 @@
 /* JavaCCOptions:TOKEN_EXTENDS=,KEEP_LINE_COL=null,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package com.homesnap.webserver.rest.parser;
 
+/*
+ * #%L
+ * HomeSnapWebServer
+ * %%
+ * Copyright (C) 2011 - 2014 A. de Giuli
+ * %%
+ * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * 
+ *     MyDomo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     MyDomo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 /**
  * Describes the input token stream.
  */
