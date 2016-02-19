ac125ebe3fcf9c2831abb8d87e01bc68fc8aacd9
adegiuli <arnaud.degiuli@free.fr>
2016-02-19 12:02:26 +0100

    Update file header

--- recorder/src/main/java/com/homesnap/recorder/Constants.java
+++ recorder/src/main/java/com/homesnap/recorder/Constants.java
@@ -1,5 +1,28 @@
 package com.homesnap.recorder;
 
+/*
+ * #%L
+ * HomeSnapRecorder
+ * %%
+ * Copyright (C) 2011 - 2016 A. de Giuli
+ * %%
+ * This file is part of HomeSnap done by A. de Giuli (arnaud.degiuli(at)free.fr).
+ * 
+ *     MyDomo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     MyDomo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 public interface Constants {
 
 	String VERSION = "1.0";
